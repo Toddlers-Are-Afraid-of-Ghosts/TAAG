19f94fa9d7d715fbe8d5ab9c301c6778f85f6369
Arnoloh <nono.corcione@gmail.com>
2022-03-05 22:04:17 +0100

    Revert "Remove Warning in Move.cs" revert This reverts commit 633c76561df7aa5d5f94f55298a6e530a366185e.

--- Game/Assets/Script/Move.cs
+++ Game/Assets/Script/Move.cs
@@ -4,21 +4,21 @@ using UnityEngine;
 
 public class Move : MonoBehaviour
 {
-    Rigidbody2D rb;
+    Rigidbody2D rigidbody;
 
     
     public int speedbase = 1000;
 
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        rigidbody = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
         float horizontal = Input.GetAxis("Horizontal") * Time.deltaTime;
         float vertical = Input.GetAxis("Vertical") * Time.deltaTime;
-        rb.velocity = new Vector2(horizontal * speedbase, vertical * speedbase);
+        rigidbody.velocity = new Vector2(horizontal * speedbase, vertical * speedbase);
 
         if (Input.GetKey(KeyCode.B))
         {
