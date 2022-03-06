2d9e7b95200f5e4d6005ff1ea6a690df64604a7b
Thomas <thomas.solatges@gmail.com>
2022-03-06 19:21:07 +0100

    initial commit

---
+++ Game/Assets/Script/RoomSpawn.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class RoomSpawn : MonoBehaviour
+{
+    public int openingDirection;
+
+    private RoomTemplates templates;
+    private int rand;
+    private bool spawned = false;
+    
+    void Start()
+    {
+        templates = GameObject.FindGameObjectWithTag("ROOMS").GetComponent<RoomTemplates>();
+        Invoke("Spawn", 0.1f);
+    }
+
+    
+    void Spawn()
+    {
+        if (!spawned)
+        {
+            if (openingDirection == 1)
+            {
+                //bottom
+                rand = Random.Range(0, templates.bottomRooms.Length);
+                Instantiate(templates.bottomRooms[rand], transform.position, quaternion.identity);
+            }
+            if (openingDirection == 2)
+            {
+                //top
+                rand = Random.Range(0, templates.topRooms.Length);
+                Instantiate(templates.topRooms[rand], transform.position, quaternion.identity);
+            }
+            if (openingDirection == 3)
+            {
+                //left
+                rand = Random.Range(0, templates.leftRooms.Length);
+                Instantiate(templates.leftRooms[rand], transform.position, quaternion.identity);
+            }
+            if (openingDirection == 4)
+            {
+                //right
+                rand = Random.Range(0, templates.rightRooms.Length);
+                Instantiate(templates.rightRooms[rand], transform.position, quaternion.identity);
+            }
+
+            spawned = true;
+        }
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Spawnpoint") && other.GetComponent<RoomSpawn>().spawned)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
