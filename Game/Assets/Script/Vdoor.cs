22e1f4718acffe954ff657d1df2fedda93f3a2a4
Thomas <thomas.solatges@gmail.com>
2022-03-08 02:32:11 +0100

    tentative de fix de camera

--- Game/Assets/Script/Vdoor.cs
+++ Game/Assets/Script/Vdoor.cs
@@ -1,20 +1,27 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
 
 public class Vdoor : MonoBehaviour
 {
     private Vector3 dest;
+    private Vector3 playerTP;
     public int movementDirection;
     public Transform mc;
+    public Transform player;
     private bool isLerp = false;
     private float Speed = 3f;
+    public float Delay = 0f;
+    
 
     
     private void Start()
     {
         mc = GameObject.FindGameObjectWithTag("MainCamera").transform;
+        player = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     private void Update()
@@ -31,29 +38,49 @@ public class Vdoor : MonoBehaviour
         {
             //bottom
             var position = mc.position;
+            var tp = player.position;
             dest = new Vector3(position.x, - 13, position.z);
+           // playerTP = new Vector3(tp.x, - 1, tp.z);
             mc.transform.position = Vector3.Lerp(position, dest, Time.deltaTime * Speed);
+            //player.transform.position = Vector3.Lerp(tp, playerTP, 100000000);
+            Delay = 10f;
+            isLerp = false;
         }
         else if (movementDirection == 2)
         {
             //top
             var position = mc.position;
+            var tp = player.position;
             dest = new Vector3(position.x, 13, position.z);
+           // player.transform.position = new Vector3(tp.x, 1, tp.z);
             mc.transform.position = Vector3.Lerp(position, dest, Time.deltaTime * Speed);
+            //player.transform.position = Vector3.Lerp(tp, playerTP, 100000000);
+            Delay = 10f;
+            isLerp = false;
         }
         else if (movementDirection == 3)
         {
             //left
             var position = mc.position;
+            var tp = player.position;
             dest = new Vector3(- 27, position.y , position.z);
+           // player.transform.position = new Vector3(-1, tp.y, tp.z);
             mc.transform.position = Vector3.Lerp(position, dest, Time.deltaTime * Speed);
+            //player.transform.position = Vector3.Lerp(tp, playerTP, 100000000);
+            Delay = 10f;
+            isLerp = false;
         }
         else if (movementDirection == 4)
         {
             //right
             var position = mc.position;
+            var tp = player.position;
             dest = new Vector3(27, position.y, position.z);
+            //player.transform.position = new Vector3(1, tp.y, tp.z);
             mc.transform.position = Vector3.Lerp(position, dest, Time.deltaTime * Speed);
+            //player.transform.position = Vector3.Lerp(tp, playerTP, 100000000);
+            Delay = 10f;
+            isLerp = false;
         }
     }
 
@@ -62,6 +89,10 @@ public class Vdoor : MonoBehaviour
         if (other.gameObject.CompareTag("Player"))
         {
             isLerp = true;
+        }
+        else
+        {
+            isLerp = false;
         }
     }
 }
